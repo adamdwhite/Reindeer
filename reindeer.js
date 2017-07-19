fad99e3ac269c2e734b743d67ce89e02e280ebd6
Adam D. White <adamwhite82@gmail.com>
2017-07-19 13:55:17 -0500

    completed the painted reindeer

--- reindeer.js
+++ reindeer.js
@@ -1,21 +1,36 @@
+console.log("lets paint the reindeer!");
+
+
 var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
 var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
 
 
-var hohohoElement = document.getElementById("reindeer");
+var hohohoElement = document.getElementById("painted_reindeer");
 
-var currentReindeer =
-    // Your task is to loop through all the reindeer in the array, 
-    // and add the name of the reindeer to the single HTML <div> element provided. 
-    // The name of the reindeer should be prepended with the corresponding color 
-    // from the other array.
+// Your task is to loop through all the reindeer in the array, 
+// and add the name of the reindeer to the single HTML <div> element provided. 
+// The name of the reindeer should be prepended with the corresponding color 
+// from the other array.
 
-    // Example output:
+// Example output:
+// Blue Dasher
+// Red Dancer
+// ...
 
-    // Blue Dasher
 
-    // Red Dancer
+if (colors.length < reindeer.length) {
+    loopCounter = colors.length;
+} else if (reindeer.length < colors.length) {
+    loopCounter = reindeer.length;
+} else {
+    loopCounter = colors.length;
+}
+// console.log("Colors Length:", colors.length);
+// console.log("Reindeer Length:", reindeer.length);
+// console.log(loopCounter);
 
-    // ...
-
-    for
+for (var i = 0; i < loopCounter; i++) {
+    hohohoElement.innerHTML += `<p>${colors[i]} ${reindeer[i]}</p>`
+    console.log(colors[i]);
+    console.log(reindeer[i]);
+}
